74c9ba75939a03eed84719e2416aee626a19300a
antsankov <antsankov@gmail.com>
2014-09-18 17:32:25 -0600

    finished first 3 challenges

--- github3.js
+++ github3.js
@@ -1,1 +1,11 @@
-// TODO:
+var restler = require('restler');
+
+restler.get('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-1/forks').on('complete', function(repos) {
+  if (repos instanceof Error) {
+    console.log('Error:', repos.message);
+    this.retry(500);
+  } 
+  else {
+    console.log(repos);
+  }
+});
