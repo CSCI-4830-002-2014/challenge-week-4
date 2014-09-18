9dfff30433acb25056278f808a6837e922d360eb
dbotsford <dawsonbotsford@gmail.com>
2014-09-18 00:22:12 -0600

    challenges 2 & 3 completed

--- github2.js
+++ github2.js
@@ -1,1 +1,10 @@
-// TODO:
+var rest = require('restler');
+
+rest.get('https://api.github.com/orgs/CSCI-4830-002-2014/repos').on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error:', result.message);
+    this.retry(1000); // try again after 1 sec
+  } else {
+    console.log(result);
+  }
+});
