aab1e9f9206659ee3132f83a59957eae0f8fb8af
dbotsford <dawsonbotsford@gmail.com>
2014-09-18 14:25:18 -0600

    more restler challenges completed

--- github6.js
+++ github6.js
@@ -1,1 +1,12 @@
-// TODO:
+var rest = require('restler');
+var options = {
+  "title": "Test issue"  
+}
+rest.post('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues?title=Test1').on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error:', result.message);
+    this.retry(1000); // try again after 1 sec
+  } else {
+    console.log(result);
+  }
+});
