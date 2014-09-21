7413af26fb16b61c775b4e89cace56bf826edc0f
Alex Tsankov <antsankov@gmail.com>
2014-09-21 12:40:50 -0600

    Fixed params issue.

--- github6.js
+++ github6.js
@@ -1,11 +1,13 @@
 var restler = require('restler');
 //this makes the json that contains our parameters to post. 
+var title = {
+	"title" : "TEST ISSUE!"
+}
 var params = {
-	"title" : "Test Issue",
 	"username": "antsankov",
 	"password": "REDACTED"
 }
-restler.postJson('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues',params).on('complete', function(repos) {
+restler.postJson('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues',title,params).on('complete', function(repos) {
   if (repos instanceof Error) {
     console.log('Error:', repos.message);
     this.retry(500);
