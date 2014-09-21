3c82cc4acedf06e1d966655f9f61313db726d525
Justin McBride <justin@dare599z.com>
2014-09-21 15:05:42 -0600

    GH6 JS & Results

--- github6.js
+++ github6.js
@@ -1,1 +1,15 @@
-// TODO:
+var pwjs = require('./password.js');
+var restler = require('restler');
+
+var auth = {
+	"username": "dare599z",
+	"password": pwjs.password
+}
+
+var params = {
+	"title": "Custom Issue",
+ }
+
+restler.postJson('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues', params, auth).on('complete', function(repos) {
+     console.log(repos);
+});
