6c8df421df29f270410c80a4ddd95889ecce643d
Justin McBride <justin@dare599z.com>
2014-09-21 14:33:10 -0600

    GH5 JS and results

--- github5.js
+++ github5.js
@@ -1,1 +1,5 @@
-// TODO:
+var restler = require("restler");
+
+restler.get('https://api.github.com/repos/LoganBates/hackathon-accesslog/stats/contributors').on('complete', function(data) {
+	console.log(data);
+});
