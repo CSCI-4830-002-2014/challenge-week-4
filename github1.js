e1a4a5f71ff3cb322003ebc8b642fee2ae6c4371
Jake <jake.white@colorado.edu>
2014-09-21 19:44:33 -0600

    Update github1.js

--- github1.js
+++ github1.js
@@ -1,1 +1,9 @@
-// TODO:
+var rest = require('restler');
+
+var getResp = function(url){
+  rest.get(url).on('complete', function(publicRepos){
+    console.log(publicRepos);
+  });
+};
+
+getResp('https://api.github.com/repositories');
