36bc1c4dff481388a0beabcf2076baa2e44923ff
LoganBates <loganbates@gmail.com>
2014-09-21 20:53:58 -0600

    API challenges done

--- github5.js
+++ github5.js
@@ -1,10 +1,24 @@
 var rest = require('restler');
+var async = require("async");
 
-rest.get('https://api.github.com/repos/LoganBates/hackathon-accesslog/stats/contributors').on('complete', function(results) {
-  if (results instanceof Error) {
-    console.log('Error:', results.message);
-    this.retry(5000);
-  } else {
-    console.log(results);
-  }
+
+function getClassEventsForPage(i, callback){
+	var page = 'https://api.github.com/orgs/CSCI-4830-002-2014/events?page=' + i;
+	restler.get(page).on('complete', function(data) {
+		console.log(data);
+	});
+};
+ 
+function flatten(input) {	
+	var flattened = [];
+	for (var i = 0; i < input.length; ++i) {
+		var current = input[i];
+		for (var j = 0; j < current.length; j++) flattened.push(current[j]);
+	}
+return flattened;
+}
+
+async.map([1,2,3,4,5,6,7,8,9,10], getClassEventsForPage, function(err, results) {
+	flattened = flatten(results);    
+	console.log(JSON.stringify(flattened, undefined, 4));
 });
--- github6.js
+++ github6.js
@@ -1,11 +1,13 @@
 var rest = require('restler');
+var postTitle = { 
+                 "title" : "IssueChallenge4",
+                }
+var login = {
+              "username" : "LoganBates",
+              "password" : "Filler"
+            }
 
-rest.post('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues', {
-	"title": "IssueChallenge4",
-	"body": "This morning I had pancakes for breakfast",
-	"username" : "LoganBates",
-	"password" : "Goes here"
-}).on('complete', function(results) {
+rest.postJson('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-4/issues', postTitle, login).on('complete', function(results) {
   if (results instanceof Error) {
     console.log('Error:', results.message);
     this.retry(5000);
