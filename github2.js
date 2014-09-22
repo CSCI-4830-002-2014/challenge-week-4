4c5acd6b6d898e651552194e674dd6d6936033e8
dano8957 <daniel.j.nolan@colorado.edu>
2014-09-21 21:49:23 -0600

    Update github2.js

--- github2.js
+++ github2.js
@@ -1,7 +1,1 @@
-var rest = require('restler');
 
-var api_request = "repos/csci-4830-002-2014/challenge-week-1/forks"
-
-rest.get('https://api.github.com/' + api_request).on('complete', function(data) {
-	console.log(data[0]);
-});
