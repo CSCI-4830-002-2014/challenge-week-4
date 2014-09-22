6346153be34b4e622424fe0725d662ffbd88146e
dano8957 <daniel.j.nolan@colorado.edu>
2014-09-21 21:50:44 -0600

    Update github4.js

--- github4.js
+++ github4.js
@@ -1,1 +1,8 @@
 // TODO:
+var rest = require('restler');
+
+var api_request = "repos/csci-4830-002-2014/challenge-week-1/pulls?state=closed"
+
+rest.get('https://api.github.com/' + api_request).on('complete', function(data) {
+	console.log(data[0]);
+});
