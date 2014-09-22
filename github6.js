112d4ccdf4214b9fcd238574c5a0a76f15b2936a
thefyk <michaelfyk@gmail.com>
2014-09-21 21:44:20 -0600

    Update github6.js

--- github6.js
+++ github6.js
@@ -1,1 +1,19 @@
-// TODO:
+rest = require('restler')
+
+
+var data = {
+"title": "This is Michael Fyk",
+"body": "New issue"	
+}
+rest.postJson('https://api.github.com/repos/CSCI-4830-002-2014/challenge-week-3/issues', data, {
+	"username": "thefyk",
+	"password": "pikle3310"
+}
+	).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error:', result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    console.log(result);
+  }
+});
